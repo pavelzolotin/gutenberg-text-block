8c3ad3a2dfa9359d755965a6a5345730b1a0513a
pavelzolotin <pavel.zolotin@goat.digital>
2022-04-19 14:16:19 +0500

    first commit

---
+++ build/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-blob', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => 'a600b18d7cf051f5a1ebf58655f2a1f0');
---
+++ src/edit.js
@@ -0,0 +1,5 @@
+import './editor.scss';
+
+export default function Edit() {
+
+}
